cfab7d8eb7417fc781a3a4a814592b781d796264
Arthur <baidragoon@gmail.com>
2017-09-01 19:25:08 +0800

    show owner of books

--- resources/views/books/item.blade.php
+++ resources/views/books/item.blade.php
@@ -1,6 +1,7 @@
 <tr>
     <td>{{ $book->id }}</td>
     <td><a href="{{ route('books.show', $book->id) }}">{{ $book->title }}</a></td>
+    <td>{{ $book->user->name }}</td>
     <td>{{ $book->description }}</td>
     <td>{{ $book->created_at }}</td>
     <td>{{ $book->updated_at }}</td>
